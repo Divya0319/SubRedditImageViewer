06e1e321933fccfb7ee95a382cbf299f5878ebdc
Divya Gupta <divygupta0319@gmail.com>
2020-07-11 19:31:24 +0530

    Comment modified in Status.kt

--- app/src/main/java/com/assignment/imagessubredditviewer/network/ApiResponse.kt
+++ app/src/main/java/com/assignment/imagessubredditviewer/network/ApiResponse.kt
@@ -8,23 +8,27 @@ import com.google.gson.JsonElement
 
 /**
  * A wrapper class created for capturing the API call responses,
- * let it be call ongoing, call returned success or, call returned error
+ * let it be call ongoing, call returned success , call returned error, or call returned empty response with success status code(200)
  */
 class ApiResponse(val status: Status, val data: JsonElement?, val error: Throwable?) {
     companion object {
 
+        // call ongoing
         fun loading(): ApiResponse {
             return ApiResponse(Status.LOADING, null, null)
         }
 
+        // call returned success
         fun success(data: JsonElement): ApiResponse {
             return ApiResponse(Status.SUCCESS, data, null)
         }
 
+        // call returned error
         fun error(error: Throwable): ApiResponse {
             return ApiResponse(Status.ERROR, null, error)
         }
 
+        // call returned empty response with success status code(200)
         fun complete(): ApiResponse {
             return ApiResponse(Status.COMPLETE, null, null)
         }
